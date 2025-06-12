78bd5497bbadbbde77d353beb459b47bced2db7a
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-12 21:15:08 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,38 +1,49 @@
 export const projectsData = [
     {
         id: 1,
+        name: 'LiveAuction - Real-Time Bidding System',
+        description: "LiveAuction is a full-stack web application that enables real-time bidding on products through a live auction platform. The system supports separate buyer and seller roles, allowing sellers to list products and buyers to participate in ongoing auctions with live countdown timers and dynamic bid updates. It includes features such as real-time email verification during registration, secure user authentication, profile management, product uploads, bid tracking, bid history, and winning history. Sellers can manage live auctions, view bid activity, and track sold items. Buyers can browse available auctions, join live bidding sessions, and monitor their bid performance. The application ensures seamless real-time data updates, enhancing user engagement, security, and bidding competitiveness.",
+        tools: ['React.js', 'Ant Design', 'CSS Modules', 'Tailwind CSS', 'Node.js',' Express.js','MongoDB','Socket.IO', 'JWT' , 'bcrypt','Render'
+
+    ],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: 'https://bidpulse-liveauction.onrender.com/',
+    },
+    {
+        id: 2,
         name: 'ChatGPT DApp',
         description: "ChatDApp is a decentralized chatbot application built using **React**, **Web3**, and **smart contracts**, designed to provide blockchain-based membership functionality. It integrates **MetaMask**, **Web3Modal**, and **ethers.js** for secure wallet authentication and interaction with the **GPTMembership** contract deployed on a **Hardhat** local network. Users can connect their wallets, access premium chatbot features based on their membership status, and experience a Web3-powered AI conversation system. The project enhances user engagement by combining AI-powered chat with decentralized authentication and membership management.",
         tools: ['Express',  'DeepSeek API', 'Node js', 'HTML', 'JAVASCRIPT', 'CSS', 'METAMASK', 'WEB3MODAL'],
         role: 'Full Stack Developer',
         code: '',
-        demo: '',
+        demo: 'https://deepconverseai-sharath.onrender.com/',
     },
     {
-        id: 2,
+        id: 3,
         name: 'Human Resource Management System (HRMS)',
         description: 'The **Human Resource Management System** is designed to streamline HR processes within an organization. It encompasses features such as employee information management, attendance tracking, leave management, and performance evaluations. The system aims to enhance operational efficiency by automating routine HR tasks, thereby reducing manual workload and minimizing errors. By providing a centralized platform for managing employee data and HR activities, the project facilitates improved decision-making and organizational productivity.',
         tools: ["PHP","CSS","JAVACRIPT","HTML",'MYSQL'],
         role: 'Full Stack Developer',
         code: '',
-        demo: '',
+        demo: 'https://deepconverseai-sharath.onrender.com/',
     },
     {
-        id: 3,
+        id: 4,
         name: 'Air-Canvas-with-ML',
         description: 'The **Air Canvas** project is a Python-based application that enables users to draw on a virtual canvas using hand gestures captured via a webcam. Leveraging **OpenCV** for computer vision and **MediaPipe** for hand tracking, the program identifies hand movements and translates them into digital drawings in real-time. Users can select different colors and clear the canvas through specific gestures, providing an intuitive and interactive drawing experience without the need for physical contact with a device.',
         tools: ['PYTHON','CV2','NUMPY','MEDIAPIPE'],
         code: '',
         role: 'Software Developer',
-        demo: '',
+        demo: 'https://deepconverseai-sharath.onrender.com/',
     },
     {
-        id: 4,
+        id: 5,
         name: 'Resume Builder',
         description: "This online resume builder assists job searchers in building a professional resume, making the task of creating a resume easier. Individuals must simply fill out a form that includes questions from all required sections, such as personal questions, educational questions, qualities, interests, and skills. Users' responses are kept, and the system generates a well-structured resume with customisable templates automatically. Users have the option of creating resumes in any format or file type.",
         tools: ['HTML','CSS','JS'],
         code: '',
-        demo: '',
+        demo: 'https://deepconverseai-sharath.onrender.com/',
         role: 'Full Stack Developer',
     }
 ];
